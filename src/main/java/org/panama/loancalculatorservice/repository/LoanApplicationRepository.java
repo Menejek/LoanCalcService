b24b2166721293da7c9c3c93aee911d7674ab06a
Menejek <bistriydaniil@yandex.ru>
2026-06-02 22:15:59 +0300

    feat: added caching in redis not compltely

--- src/main/java/org/panama/loancalculatorservice/repository/LoanApplicationRepository.java
+++ src/main/java/org/panama/loancalculatorservice/repository/LoanApplicationRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface LoanApplicationRepository extends JpaRepository {
+public interface LoanApplicationRepository extends JpaRepository<LoanApplication, UUID> {
 
     Optional<LoanApplication> findByIdempotencyKey(UUID idempotencyKey);
 
