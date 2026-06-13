2985ba6a15c0b3aedba5dccd51911d45895e0d9e
Menejek <bistriydaniil@yandex.ru>
2026-06-13 18:14:40 +0300

    fix:typos have been fixed, rollback and default time have been added, and a status index has been created

--- src/main/resources/db/changelog/changesets/2026-13-06--003-create-outbox-table.sql
+++ src/main/resources/db/changelog/changesets/2026-13-06--003-create-outbox-table.sql
@@ -7,12 +7,14 @@ CREATE TABLE outbox (
                     payload JSONB NOT NULL,
                     event_type VARCHAR(255) NOT NULL,
                     status VARCHAR(255) CHECK (status IN(
-                                                        'UNDER_REVIEW',
-                                                        'APPROVED',
-                                                        'REJECTED',
-                                                        'DOCUMENTS_PENDING',
-                                                        'NEEDS_MANUAL_REVIEW'
+                                                        'PENDING',
+                                                        'SEND',
+                                                        'FAILED'
 
                     )),
-                    crated_at TIMESTAMP(6) NOT NULL
+                    created_at TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP
                 );
+
+CREATE INDEX idx_outbox_status ON outbox (status);
+
+--rollback DROP TABLE outbox
